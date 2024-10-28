68f9cefe8e6702bbef8cb72827a1f9d3df8c9db1
sequoya126 <sequoya.macleanc@gmail.com>
2024-10-27 22:43:52 -0400

    Update pricing.js

--- pricing.js
+++ pricing.js
@@ -1,12 +1,1 @@
-//Se code est plus important en se momnent
-const pricingPlan = document.querySelector('.pricing-plan');
 
-// Start animation on mouse over
-pricingPlan.addEventListener('mouseover', () => {
-    pricingPlan.style.animationPlayState = 'running';
-});
-
-// Stop animation on mouse out
-pricingPlan.addEventListener('mouseout', () => {
-    pricingPlan.style.animationPlayState = 'paused';
-});
